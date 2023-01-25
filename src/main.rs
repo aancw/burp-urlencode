285b1a7227000ce4c8452a5db1f1ae3382bf45c7
Aan <me@petruknisme.com>
2023-01-26 02:02:58 +0700

    Initial release

---
+++ src/main.rs
@@ -0,0 +1,66 @@
+// Copyright (c) 2022 Petruknisme
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+extern crate clap;
+extern crate colored;
+
+use clap::Parser;
+use colored::Colorize;
+
+
+#[derive(Parser)]
+#[clap(name = "burp-urlencode")]
+#[clap(author = "Petruknisme <me@petruknisme.com>")]
+#[clap(version = "1.0")]
+#[clap(about = "Burp Suite URL encoding all characters implementation in Rust", long_about = None)]
+
+struct Cli {
+    /// Text to be encoded
+    #[clap(short, long)]
+    text: String,
+
+    /// How many time text will be url encoded
+    #[clap(short, long)]
+    iteration: i32,
+}
+
+struct Encode {
+    value: String,
+}
+
+fn urlencode(text: &str) -> String {
+    let mut hex = String::new();
+    
+    for c in text.chars() {
+       hex.push_str(&format!(" {:x}", c as u32));
+    }
+    hex.replace(" ", "%")
+}
+
+fn main() {
+    let cli = Cli::parse();
+    let text = cli.text;
+    let i = cli.iteration;
+    
+    println!("{}", "
+    Burp Suite URL encoding all characters implementation in Rust
+    by Petruknisme
+    ".yellow());
+    println!("{}", "[+] Encoding...".yellow());
+    println!("Text: {}", &text);
+    println!("Iteration: {}", &i);
+
+    let mut encode = Encode { value: text.to_string() };
+
+    for n in 0..i {
+        if n == 0 {
+            encode.value = urlencode(&text);
+        }else{
+            encode.value = urlencode(&encode.value);
+        }
+    }
+    println!("{}", "[+] Result: ".green());
+    println!("{}", encode.value);
+}
